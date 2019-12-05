77f3287a1f3726578bb173e602012a53cbd7bdd8
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-12-06 10:45:34 +1100

    Import intcode code

--- 5/intcode.rb
+++ 5/intcode.rb
@@ -1,5 +1,45 @@
-require_relative '../2/intcode'
+class Intcode
+  attr_reader :program
 
-class IntcodeMkII < Intcode
+  def initialize program
+    @program = load_program
+    @pointer = 0
+  end
 
+  def run
+    until @program[@pointer] == 99
+      case @program[@pointer]
+      when 1
+        opcode_one
+      when 2
+        opcode_two
+      else
+        break
+      end
+      @pointer += 4
+    end
+    @program
+  end
+
+  def get_addr index
+    @program[@pointer + index]
+  end
+
+  def opcode_one
+    @program[get_addr 3] = @program[get_addr 1] + @program[get_addr 2]
+  end
+
+  def opcode_two
+    @program[get_addr 3] = @program[get_addr 1] * @program[get_addr 2]
+  end
+
+  def load_base_program
+    program = []
+    File.open './input' do |file|
+      file.each do |line|
+        program = line.chomp.split(',').map { |str| str.to_i }
+      end
+    end
+    program
+  end
 end
