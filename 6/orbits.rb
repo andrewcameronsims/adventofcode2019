bdfae288dfc9d7e860e33b62b76f0e5af50b77ce
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-12-07 13:27:47 +1100

    WIP

--- 6/orbits.rb
+++ 6/orbits.rb
@@ -1,3 +1,42 @@
-class OrbitCalculator
-  
+require 'pry'
+
+class OrbitMap
+  attr_reader :orbits
+
+  def initialize filename
+    @data = load_data filename
+    @orbits = {}
+    direct_orbits @data
+  end
+
+  def direct_orbits data
+    data.each do |orbit|
+      oed, oer = orbit.split(")")
+      @orbits[oer] = [oed]
+    end
+  end
+
+  def count_orbits
+    indirects = 0
+    @orbits.each do |oer, oed|
+      while @orbits[oer]
+        oer = @orbits[oer]
+        indirects += 1
+      end
+    end
+    indirects
+  end
+
+  def load_data filename
+    data = []
+    File.open filename do |file|
+      file.each do |line|
+        data << line.chomp
+      end
+    end
+    data
+  end
 end
+
+om = OrbitMap.new './input'
+p om.count_orbits
