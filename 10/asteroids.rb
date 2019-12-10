123f3badba559f658dcfc8e371a8406797f86f22
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-12-10 17:08:54 +1100

    Do the easy stuff.

--- 10/asteroids.rb
+++ 10/asteroids.rb
@@ -1,6 +1,30 @@
 class AsteroidField
+  attr_reader :candidates, :field
+
   def initialize filename
     @field = load_field filename
+    @candidates = generate_stations
+  end
+
+  def generate_stations
+    stations = []
+    rows = @field.length
+    cols = @field[0].length # assuming equal row lengths
+    (1...rows).each do |row|
+      (1...cols).each do |col|
+        stations << Station.new(row, col) if asteroid? row, col
+      end
+    end
+    stations
+  end
+
+  def asteroid? row, col
+    @field[row][col] == "#"
+  end
+
+  def most_asteroids_detectable
+    detectable = stations.map { |station| station.detect_asteroids }
+    detectable.max
   end
 
   def load_field filename
@@ -15,6 +39,15 @@ class AsteroidField
 end
 
 class Station
+  def initialize row, col
+    @row = row
+    @col = col
+  end
+
+  def detect_asteroids field
+  end
 end
 
 af = AsteroidField.new './input'
+puts af.field
+puts af.candidates.length
